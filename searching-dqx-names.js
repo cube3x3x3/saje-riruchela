b66c3b4ad034d53ba3be452c42f1a3fdab334e2d
cube3x3x3 <cube3x3x3@gmail.com>
2017-05-19 10:32:47 +0900

    Change to module style
    Because it does not collide with the function name of other script.

--- searching-dqx-names.js
+++ searching-dqx-names.js
@@ -1,80 +1,87 @@
+var searchDqxNames = searchDqxNames || {};
 
-var item = {
-  name : "test",
-  hasHiraKata : function(){
-    // [\u3040-\u309F] hiragana
-    // [\u30A0-\u30FF] katakana
-    if (this.name.match(/^[\u3040-\u30FF]+$/)) {
-      return true;
-    }else{
-      return false;
-    }
-  }
-};
-
-function cleanChildNodes(node){
-  if (node.hasChildNodes()){
-    while (node.firstChild){
-      node.removeChild(node.firstChild);
-    }
-  }
-}
-
-function createUL(list){
-  var newUL = document.createElement("ul");
-  for (var i = 0; i < list.length; i++){
-    var li = document.createElement("li");
-    li.textContent = list[i];
-    newUL.appendChild(li);
-  }
-  return newUL;
-}
-
-function appendHitNames(outputArea, inputReg, omitKanji, jsonNamefile){
-  //Load a search target name file.
-  var request = new XMLHttpRequest();
-  request.open('GET', jsonNamefile);
-  request.onload = function(){
-    //Finish load. start Searching. and Output.
-    var nameArray = JSON.parse(this.responseText);
-    var results = searchHitNames(inputReg, omitKanji, nameArray);
-    var newUL = createUL(results);
-    outputArea.appendChild(newUL);
-  };
-  request.send();
-}
-
-function searchDqxNames(outputArea, inputReg, omitKanji, location){
-  if (!location) {
-    location = "Data/";
-  }
-  //const prefix = "Data/";
-  const suffix = "Name.json";
-  const namefiles = ["monster", "magic", "arms", "item", "foods", "material", "skill", "special", "chance", "accessory", "place", "facilities"];
-  cleanChildNodes(outputArea);
-  //Not sequential. becouse use XMLHttpRequest.
-  for (var i = 0; i < namefiles.length; i++){
-    var fileName = location + namefiles[i] + suffix;
-    // ex. Data/monsterName.json
-    appendHitNames(outputArea, inputReg, omitKanji, fileName);
-  }
-}
-
-function searchHitNames(inputReg, omitKanji, targetArray){
-  //window.alert(inputReg.value);
-  //window.alert(omitKanji);
-  if (!inputReg.value){return;}
-
-  re = new RegExp(inputReg.value);
-  var results = [];
-
-  for (var i = 0; i < targetArray.length; i++){
-    item.name = targetArray[i];
-    if (re.test(item.name)){
-      if(!omitKanji || item.hasHiraKata()){
-        results.push(item.name);
+searchDqxNames.exec = function(outputArea, inputReg, omitKanji, opt_location){
+  function cleanChildNodes(node){
+    if (node.hasChildNodes()){
+      while (node.firstChild){
+        node.removeChild(node.firstChild);
       }
     }
   }
-  return results;
-}
+  
+  function createUL(list){
+    var newUL = document.createElement("ul");
+    for (var i = 0; i < list.length; i++){
+      var li = document.createElement("li");
+      li.textContent = list[i];
+      newUL.appendChild(li);
+    }
+    return newUL;
+  }
+  
+  function searchHitNames(inputReg, omitKanji, targetArray){
+    var item = {
+      name : "test",
+      hasHiraKata : function(){
+        // [\u3040-\u309F] hiragana
+        // [\u30A0-\u30FF] katakana
+        if (this.name.match(/^[\u3040-\u30FF]+$/)) {
+          return true;
+        }else{
+          return false;
+        }
+      }
+    };
+    //window.alert(inputReg.value);
+    //window.alert(omitKanji);
+    if (!inputReg.value){return;}
+  
+    re = new RegExp(inputReg.value);
+    var results = [];
+  
+    for (var i = 0; i < targetArray.length; i++){
+      item.name = targetArray[i];
+      if (re.test(item.name)){
+        if(!omitKanji || item.hasHiraKata()){
+          results.push(item.name);
+        }
+      }
+    }
+    return results;
+  }
+
+  function appendHitNames(outputArea, inputReg, omitKanji, jsonNamefile){
+    //Load a search target name file.
+    var request = new XMLHttpRequest();
+    request.open('GET', jsonNamefile);
+    request.onload = function(){
+      //Finish load. start Searching. and Output.
+      var nameArray = JSON.parse(this.responseText);
+      var results = searchHitNames(inputReg, omitKanji, nameArray);
+      var newUL = createUL(results);
+      outputArea.appendChild(newUL);
+    };
+    request.send();
+  }
+  
+
+  var location = "Data/";
+  var NAME_FILES = ["monster", "magic", "arms", "item", "foods", "material", "skill", "special", "chance", "accessory", "place", "facilities"];
+  var SUFFIX = "Name.json";
+
+  if (opt_location) {
+    location = opt_location;
+  }
+  //const prefix = "Data/";
+  cleanChildNodes(outputArea);
+  //Not sequential. becouse use XMLHttpRequest.
+  for (var i = 0; i < NAME_FILES.length; i++){
+    var fileName = location + NAME_FILES[i] + SUFFIX;
+    // ex. Data/monsterName.json
+    appendHitNames(outputArea, inputReg, omitKanji, fileName);
+  }
+};
+
+
+
+
